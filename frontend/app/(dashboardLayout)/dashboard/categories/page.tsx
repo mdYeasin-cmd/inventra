3eaca1115035ad583f8156893b8c18556e9c13c8
mdYeasin-cmd <yeasin200011@gmail.com>
2026-04-01 09:46:56 +0600

    setup necessary frontend route

---
+++ frontend/app/(dashboardLayout)/dashboard/categories/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const CategoriesPage = () => {
+  return (
+    <div>
+      <h1>Categories Page</h1>
+    </div>
+  )
+}
+
+export default CategoriesPage
+;<h1>Categories Page</h1>
---
+++ frontend/app/(dashboardLayout)/dashboard/restock-queue/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RestockQueuePage = () => {
+  return (
+    <div>
+      <h1>Restock Queue Page</h1>
+    </div>
+  )
+}
+
+export default RestockQueuePage
